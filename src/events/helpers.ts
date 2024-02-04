6cd0decd031eb630422bdc0d1956b445189f8c8e
Anshumanv36 <anshumanmishra.v36@gmail.com>
2024-02-04 12:17:29 +0530

    events

---
+++ src/events/helpers.ts
@@ -0,0 +1,11 @@
+import EventsDb from './db';
+import { IEvent } from './interface';
+import logger, { LogTypes } from '../utils/logger';
+
+export default class EventsHelpers extends EventsDb {
+  public getAllEventsHelper = async (): Promise<IEvent[]> => {
+    logger('getAllEventsHelpers1', LogTypes.LOGS);
+    const events = await this.fetchAllEvents();
+    return events;
+  };
+}
---
+++ src/events/interface.d.ts
@@ -0,0 +1,21 @@
+export type IResponse<T = any> = {
+  success: boolean;
+  message?: string;
+  message_code: string;
+  data?: T | null;
+};
+
+export type UUID = string;
+export type EventScope = 'srm' | 'non-srm' | 'both';
+
+export type IEvent = {
+  id: UUID;
+  event_code: string;
+  name: string;
+  is_group_event: boolean;
+  event_scope: EventScope;
+  club_name: string;
+  max_group_size: number;
+  created_at: Date;
+  updated_at: Date;
+};
