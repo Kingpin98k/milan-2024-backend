22aea23c3bffba00f1c85fb5eda7cf31650a13a1
Ankit kumar <ankit.kumar.info22@gmail.com>
2024-02-10 16:18:37 +0530

    staff changes

--- src/staff/controllers.ts
+++ src/staff/controllers.ts
@@ -18,7 +18,7 @@ export default class StaffController extends StaffService {
 		try {
 			const method = req.method;
 			const routeName = req.route.path.split("/")[1];
-
+			console.log("routeName", routeName);
 			let response: IResponse = {
 				success: false,
 				data: {},
