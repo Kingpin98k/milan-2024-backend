5f5fe9c60d7b357e54706fd90c1d1bf0c064d903
ab3649 <ab3649@srmist.edu.in>
2024-02-15 09:04:20 +0530

    enum changes

--- src/events/enums.ts
+++ src/events/enums.ts
@@ -4,9 +4,9 @@ export enum EventRoutes {
   CLUB = 'club',
   EVENT_CODE = ':code',
   EVENT = 'event',
-  GET_EVENT_BY_CLUB = 'geteventbyclub',
-  GET_ALL_USERS_BY_CODE = 'getallusersbycode',
-  GET_COUNT_BY_CODE = 'getcountbycode',
+  GET_EVENT_BY_CLUB = 'getEventByClub',
+  GET_ALL_USERS_BY_CODE = 'getAllUsersByCode',
+  GET_COUNT_BY_CODE = 'getCountByCode',
   UPDATE_MAX_CAP = 'updateMaxCap',
-  ACTIVATE_EVENT = 'activate-event',
+  ACTIVATE_EVENT = 'activateEvent',
 }
